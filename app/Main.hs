e63ac4c241d6127d1d25089512390cddb2fd8114
Artem Chernyak <artem.chernyak@ocelotconsulting.com>
2019-07-02 19:23:35 -0500

    code golf round 1

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,4 @@ module Main where
 import System.Environment (getArgs)
 
 main :: IO ()
-main = do
-  let
-    getFilename :: IO FilePath
-    getFilename = do
-      args <- getArgs
-      return (head args)
-  fileName <- getFilename
-  contents <- readFile fileName
-  putStrLn contents
+main = (head <$> getArgs) >>= readFile >>= putStrLn
